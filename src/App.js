06f9d019dc4f7c8dd51e4340eed55a42d01f4386
Kimberly Izatt <kimizatt@Kims-MacBook-Pro.local>
2019-07-08 15:03:04 -0600

    Done without Black Diamond

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import { HashRouter as Router } from "react-router-dom";
 import routes from "./routes";
 import "./reset.css";
 import "./App.css";
+//import store, {UPDATE_NAME, UPDATE_CATEGORY} from './store'
+
+
+
+
+
 
 class App extends Component {
+  
+
   render() {
     return (
       <Router>
